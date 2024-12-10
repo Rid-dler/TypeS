b5748182aa092e23a166dd0ebaa069ca53c6ecf3
Riddler <reza_karimian73@yahoo.com>
2024-12-10 15:24:16 +0330

    first TS commit

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+console.log('Hello knsvdsworld');
+let age = 25;
+if (age < 100)
+    age += 10;
+console.log(age);
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,8 @@
+console.log('Hello knsvdsworld');
+let age: number = 25;
+
+
+if (age<100)
+    age += 10;
+
+console.log(age);
